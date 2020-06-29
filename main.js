bae1ee4efbcee183c0265c3ab5861d80eb129187
Lens <lensmutombo@gmail.com>
2020-06-29 16:15:54 +0200

    Changed alert to console log

---
+++ main.js
@@ -0,0 +1,7 @@
+function getIdentite(nom, postnom, prenom, numeroPhone) {
+    alert("Votre nom est " + nom + ", votre postnom est " + postnom + ", votre prenom est " + prenom + ", et enfin votre numero de telephone est le " + numeroPhone);
+}
+var nom = 'Mutombo';
+var postnom = 'Lukusa';
+var prenom = 'Lens';
+var numeroPhone = '+243977551835';
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 
 function getIdentite(nom, postnom, prenom, numeroPhone){
-    alert("Votre nom est "+nom+", votre postnom est "+postnom+", votre prenom est "+prenom+", et enfin votre numero de telephone est le "+numeroPhone);
+    console.log("Votre nom est "+nom+", votre postnom est "+postnom+", votre prenom est "+prenom+", et enfin votre numero de telephone est le "+numeroPhone);
 }
 
 var nom = 'Mutombo';
